61194a15b725b374dd33db601754c1bbc152238a
rejije <45389740+rejije@users.noreply.github.com>
2018-12-06 16:28:34 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,33 @@
-//#include "stdlib.h" "tchar.h"
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include "stdio.h"
 
-int main(int argc, char* argv[])
+
+int main()
 {
-    for(int i=0; i<argc; i++) {
-      printf("%s\n", argv[i]);   
-    }
+    bool con1,con2,con3,con4;
     
+  const unsigned short year=3200;
+  unsigned short days=365;
     
-   /* int colichestvo_zvyozdochek=8;
+    con1=((year%4)==0);
+    con2=((year%100)!=0);
+    con3=((year%400)==0);
+//  con4=(con1&&con2)||((!con2)&&con3);             //(con3)&&(!con2)    con2||con3 con3||con2
+    con4=(con1&&con2)||(con3);
+    days=days + short(con4);
     
-    char asd[2]={'*'};
-    for (int i=0; i<colichestvo_zvyozdochek; i++){                      Типы в языках программирования
-Книга, Бенджамин Пирс
-      printf(asd);
-    }
-    */
+//    auto i="In";
+//    auto y="year = ";
+//    auto d="days";
     
-   // char asd[]={'*','*','*','*', '*', '*', '*','\0'};
-    char asd[]="*********"; 
-    printf(asd);
-    
-    for(int i=argc; i>0; i--) {
-      printf("%s\n", argv[i]);   
-    }
+    printf("In %d year = %d days", year, days);
 return 0;
 }
-
-
 
