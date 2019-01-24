90cc3facedef32e6a308644db9368befb286dbb6
rejije <45389740+rejije@users.noreply.github.com>
2019-01-24 13:38:22 +0300

    Add files via upload

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	setlocale(LC_CTYPE, ".1251");
+	int a, b, c = 0, e=0, d = 0, f = 1;
+	std::cout << "Введите число.....";
+	std::cin >> a;
+	system("cls");
+	int  x = a;
+	while (a > 0) {
+		b = a % 10;
+		d += 1;
+		if (b == 0) { c += 1; }
+		e += b;
+		a = a / 10;
+	}
+	while (f != 0) {
+		std::cout << "Выберите действие для числа "<<x<<std::endl
+			<< "1.Определить количество цифр в этом числе \n"
+			<< "2.Постчитать сумму цифр этого числа \n"
+			<< "3.Посчитать среднее арифметическое цифр введенного числа \n"
+			<< "4.Определить количество нулей в этом числе \n"
+			<< "0.Выход \n"
+			<< "Ваш выбор......";
+		std::cin >> f;
+		system("cls");
+
+		switch (f) {
+		case 1: {
+			std::cout << "В числе " << x << " - " << d << " цифр" << std::endl;
+			break;
+		}
+		case 2: { 
+			std::cout << "Сумма цифр числа " << x << " равна " << e << std::endl;
+			break;
+		}
+		case 3: { 
+			std::cout << "Среднее арифметическое цифр числа " << x << " равно " << (e / (double)d) << std::endl;
+			break;
+		}
+		case 4: { 
+			std::cout << "В числе " << x << " - " << c << " нулей" << std::endl;
+			break;
+		}
+		case 0: {
+			std::cout << "Bye-Bye!" << std::endl;
+			continue;
+		}
+		default: { 
+			std::cout << "Такой команды не существует, пожалуйста выберите из списка ниже " << std::endl;
+			continue;
+		}
+
+
+		}
+
+		system("pause");
+		system("cls");
+
+		std::cout << "Хотите продожить? \n"
+			<< "1. Продолжить \n"
+			<< "0. Выход \n"
+			<<"Ваш выбор....";
+		std::cin >> f;
+		if (f == 1) {
+			system("cls");
+			continue;
+		} else {
+			system("cls");
+			std::cout<< "Bye-Bye!" << std::endl;
+		}
+	}
+	return 0;
+}
---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,241 @@
+﻿#include <iostream>
+#include "time.h"
+#include "stdlib.h"
+
+int main()
+{
+	setlocale(LC_CTYPE, ".1251");
+	srand(time(nullptr));
+	unsigned short a = 0, b = 0;
+	int d=6, c=1;
+	std::cout << "let's play" << std::endl;
+	system("pause");
+
+	for (int i = 1; i <= 3; i++) {
+
+
+		int hum1 = (int)(((float)rand() / (float)RAND_MAX) * (d - c)) + c;
+		int hum2 = (int)(((float)rand() / (float)RAND_MAX) * (d - c)) + c;
+
+		int cpu1 = (int)(((float)rand() / (float)RAND_MAX) * (d - c)) + c;
+		int cpu2 = (int)(((float)rand() / (float)RAND_MAX) * (d - c)) + c;
+
+
+
+		system("cls");
+		std::cout << "Бросок человека №" <<i<< std::endl;
+
+		switch (hum1) {
+		case 1: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "|  *  |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 2: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "| * * |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 3: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "| *** |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 4: { std::cout << " -----\n"
+			<< "| * * |\n"
+			<< "|     |\n"
+			<< "| * * |\n"
+			<< " -----\n";
+			break;
+		}
+		case 5: { std::cout << " -----\n"
+			<< "| * * |\n"
+			<< "|  *  |\n"
+			<< "| * * |\n"
+			<< " -----\n";
+			break;
+		}
+		case 6: { std::cout << " -----\n"
+			<< "| *** |\n"
+			<< "|     |\n"
+			<< "| *** |\n"
+			<< " -----\n";
+			break;
+		}
+
+		}
+
+		switch (hum2) {
+		case 1: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "|  *  |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 2: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "| * * |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 3: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "| *** |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 4: { std::cout << " -----\n"
+			<< "| * * |\n"
+			<< "|     |\n"
+			<< "| * * |\n"
+			<< " -----\n";
+			break;
+		}
+		case 5: { std::cout << " -----\n"
+			<< "| * * |\n"
+			<< "|  *  |\n"
+			<< "| * * |\n"
+			<< " -----\n";
+			break;
+		}
+		case 6: { std::cout << " -----\n"
+			<< "| *** |\n"
+			<< "|     |\n"
+			<< "| *** |\n"
+			<< " -----\n";
+			break;
+		}
+
+		}
+	//	system("pause");
+
+	//	system("cls");
+		std::cout << "Бросок компьютера №" <<i<< std::endl;
+
+		switch (cpu1) {
+		case 1: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "|  *  |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 2: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "| * * |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 3: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "| *** |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 4: { std::cout << " -----\n"
+			<< "| * * |\n"
+			<< "|     |\n"
+			<< "| * * |\n"
+			<< " -----\n";
+			break;
+		}
+		case 5: { std::cout << " -----\n"
+			<< "| * * |\n"
+			<< "|  *  |\n"
+			<< "| * * |\n"
+			<< " -----\n";
+			break;
+		}
+		case 6: { std::cout << " -----\n"
+			<< "| *** |\n"
+			<< "|     |\n"
+			<< "| *** |\n"
+			<< " -----\n";
+			break;
+		}
+
+		}
+
+		switch (cpu2) {
+		case 1: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "|  *  |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 2: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "| * * |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 3: { std::cout << " -----\n"
+			<< "|     |\n"
+			<< "| *** |\n"
+			<< "|     |\n"
+			<< " -----\n";
+			break;
+		}
+		case 4: { std::cout << " -----\n"
+			<< "| * * |\n"
+			<< "|     |\n"
+			<< "| * * |\n"
+			<< " -----\n";
+			break;
+		}
+		case 5: { std::cout << " -----\n"
+			<< "| * * |\n"
+			<< "|  *  |\n"
+			<< "| * * |\n"
+			<< " -----\n";
+			break;
+		}
+		case 6: { std::cout << " -----\n"
+			<< "| *** |\n"
+			<< "|     |\n"
+			<< "| *** |\n"
+			<< " -----\n";
+			break;
+		}
+
+		}
+		if ((hum1 + hum2) != (cpu1 + cpu2)) {
+			if ((hum2 + hum1) > (cpu2 + cpu1)) {
+				std::cout << "Вы победили!" << std::endl;
+				a += 1;
+			}
+			else { 
+				std::cout << "Вы проиграли!" << std::endl;
+				b += 1;
+			}
+		}
+		else {
+			std::cout << "Ничья!" << std::endl;
+			a += 1;
+			b += 1;
+		}
+
+		system("pause");
+	}
+
+	 system("cls");
+	 
+	 if (a != b)
+		 a > b ? std::cout << "Вы победили со счетом " <<a<<" : "<<b<<std::endl : std::cout << "Вы проиграли со счетом " <<a<<" : "<<b<<  std::endl;
+	 else  std::cout << "Ничья!" << std::endl;
+
+	return 0;
+}
