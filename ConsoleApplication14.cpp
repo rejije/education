0d016bac0622d6895c896ad0a9b539e1c5d50a06
Andrey.Bashkirov <rejije@yandex.ru>
2019-02-05 21:30:10 +0300

    asd

---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int N = 5;
+	int a = 0,k=0, arr[N] = { 5, 1, 4, 2, 6 };
+
+		for (int i = 0; i <N; i++){
+	//		for(int j=0; j<N/2; j+=2 )
+			if ((i+1)!= N && arr[i] > arr[i + 1]) {
+				a = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = a;
+			}
+	}
+		
+	for (int i = 0; i < N; i++)
+		std::cout << arr[i] << " ";
+	return 0;
+}
